de2c13b77265274ed6480504c9cb0d0872913ecd
IrynaZolkina <wzyz7557@hotmail.com>
2020-05-02 09:24:35 -0700

    Updates

---
+++ precache-manifest.cb46f9ae9e8a7ed76467da15c1c7abda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b49de2c1e52873f236b06a2118fae8",
+    "url": "/kapriz-zhytomyr/index.html"
+  },
+  {
+    "revision": "b1a2d2e75ade93c2a003",
+    "url": "/kapriz-zhytomyr/static/css/main.f7efa894.chunk.css"
+  },
+  {
+    "revision": "3f7c239c00ad0e2644e5",
+    "url": "/kapriz-zhytomyr/static/js/2.000b2387.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/kapriz-zhytomyr/static/js/2.000b2387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1a2d2e75ade93c2a003",
+    "url": "/kapriz-zhytomyr/static/js/main.c77d3442.chunk.js"
+  },
+  {
+    "revision": "5983a8179c635942699c",
+    "url": "/kapriz-zhytomyr/static/js/runtime-main.67d3e931.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/kapriz-zhytomyr/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/kapriz-zhytomyr/static/media/shopping-bag.30f07071.svg"
+  }
+]);
